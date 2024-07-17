b78bdcfc516ac6524e65c0ba757de4502be55a67
Respect Murimi <respectmurimi2000@gmail.com>
2024-07-17 11:30:54 +0200

    Show the structure of the main file

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+/*
+ * to compile oor execute the code above we use the commands below
+ * 
+ * javac Zoo.java
+ * java Zoo
+ */
